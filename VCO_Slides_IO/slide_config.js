07db78802eede28ea99d9706b435d1c21e999d9e
Gregory Kielian <gregory.kielian@gmail.com>
2013-06-03 13:19:27 -0600

    slidedeck

---
+++ VCO_Slides_IO/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'An Android DMM <br> for less than a Burrito',
+    subtitle: 'Open Software/Hardware',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Gregory Kielian',
+    company: 'Hardware && App Developer <br> ',
+	gplus: 'https://plus.google.com/110231381937100857418/',
+    twitter: '@turnbasedsoccer',
+    www: 'http://www.kielian.net',
+    github: 'http://github.com/gkielian'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
